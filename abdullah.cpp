dbb699baf1d150f836112016cfb150de17a6135b
shahzaibexe <shahzaib.exe@gmail.com>
2017-05-17 15:52:11 +0500

    ist program

---
+++ abdullah.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<      "~~~~~* wElC0M3 T0 D3vC++ *~~~~~"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"~~~~~~~ BOARD OF INTERMEDIATE & SECONDERY EDUCATION SAHIWAL ~~~~~~~~"<<	endl;
+	cout<<"sr.NO_____________.              Roll NON__613117____."<<endl;
+	cout<<"            **(PROVISONAL RESULT INTERMEDIATE)** "<<endl;
+	cout<<"           !==GroUp pRe- EnGenRiNrG==!"<<endl;
+	cout<<" name:              MUHAMMAD abdullah "<<endl;
+	cout<<"Father name :       KHUDA BUKSH"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	int urdu,eng,isl,phy,chmes,math,sum,per;
+	cout<<"Enter the no of 1st subject="<<endl;
+	cin>> urdu;
+	cout<<"Enter the no of 2nd subject="<<endl;
+	cin>> eng;
+	cout<<"Enter the no of 3rd subject="<<endl;
+	cin>> isl;
+	cout<<"Enter the no of 4th subject="<<endl;
+	cin>> phy;
+	cout<<"Enter the no of 5th subject="<<endl;
+	cin>> chmes;
+	cout<<"Enter the no of 6th subject="<<endl;
+	cin>>math;
+                      sum=urdu+eng+isl+phy+chmes+math;
+	cout<<" |sr.NO |  SUbJeCt  |  mAxImUm  |  ObTaInEd     StAtuS"<<endl;
+	cout<<" | 1    |  Urdu     |  100      |  "<<urdu<<"  |      pass"<<endl;
+	cout<<" | 2    |  endlish  |  100      |  "<<eng<<"  |      pass"<<endl;
+	cout<<" | 3    |  islamic  |  100      |  "<<isl<<"  |      pass"<<endl;
+	cout<<" | 4    |  phy      |  100      |  "<<phy<<"  |      pass"<<endl;
+	cout<<" | 5    |  chemis   |  100      |  "<<chmes<<"  |      pass"<<endl;
+	cout<<" | 6    |  math     |  100      |  "<<math<<"  |      pass"<<endl;
+	cout<<"   TOTAL marKS ARE  |  550=       ";
+    cout<<sum;
+    per=(sum*100)/600;
+    cout<<endl;
+    cout<<"~~~ The per is ~~~";
+    cout<<per;
+    cout<<"%";
+    cout<<endl;
+    cout<<endl;
+    cout<<"        THe cAnDiDaT3 hAs bEeN PaAsSeD Fr0M 1St pArT"<<endl;
+    cout<<"                (@-@) <3 ThAnK YoU <3 (@-@)"<<endl;
+    
+    
+		
+	
+	
+	
+	
+	
+	
+	
+	
+}
